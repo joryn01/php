2de2253b994d3875bd0c5fbcde8073653e373861
joryn04 <joryn04@gmail.com>
2025-03-28 06:43:36 +0800

    okay

--- PHP3/index.php
+++ PHP3/index.php
@@ -4,7 +4,6 @@
     <form action="redirect.php" method="POST">
         <h2>Register Person</h2>
         <table>
-            
             <tr>
                 <td>
                     <label for="fname">First Name</label>
@@ -29,6 +28,55 @@
                 </td>
                 <td>
                     <input type="text" name="lname" id="lname" placeholder="Enter Last Name">
+                </td>
+            </tr>
+
+            <tr>
+                <td>
+                    <label for="age">Age</label>
+                </td>
+                <td>
+                    <input type="number" name="age" id="age" placeholder="Enter Age">
+                </td>
+            </tr>
+
+            <tr>
+                <td>
+                    <label for="gender">Gender</label>
+                </td>
+                <td>
+                    <select name="gender" id="gender">
+                        <option value="male">Male</option>
+                        <option value="female">Female</option>
+                        <option value="other">Other</option>
+                    </select>
+                </td>
+            </tr>
+
+            <tr>
+                <td>
+                    <label for="email">Email</label>
+                </td>
+                <td>
+                    <input type="email" name="email" id="email" placeholder="Enter Email">
+                </td>
+            </tr>
+
+            <tr>
+                <td>
+                    <label for="address">Address</label>
+                </td>
+                <td>
+                    <textarea name="address" id="address" placeholder="Enter Address"></textarea>
+                </td>
+            </tr>
+
+            <tr>
+                <td>
+                    <label for="contact">Contact Number</label>
+                </td>
+                <td>
+                    <input type="tel" name="contact" id="contact" placeholder="Enter Contact Number">
                 </td>
             </tr>
 
